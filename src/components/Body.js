551e581a6d7daddaa6765f87deba5c1b45e5d05f
tushar <tushar.acharrya@gmail.com>
2024-09-05 19:07:35 +0530

    major changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,59 +1,24 @@
-import React, { useEffect } from 'react'
-import Login from './Login';
-import Browse from './Browse';
-import { createBrowserRouter} from 'react-router-dom';
-import { RouterProvider } from 'react-router-dom';
-import { onAuthStateChanged } from 'firebase/auth';
-import { addUser, removeUser } from '../utils/userSlice';
-import { useDispatch } from 'react-redux';
-import { auth } from '../utils/firebase';
+import { createBrowserRouter } from "react-router-dom";
+import Browse from "./Browse";
+import Login from "./Login";
+import { RouterProvider } from "react-router-dom";
 
 const Body = () => {
-const dispatch = useDispatch();
-
-
-const appRouter=createBrowserRouter([
-{
-    path:"/",
-    element:<Login/>
-},
-{
-    path:"/browse",
-    element:<Browse/>
-}
-
-
-]);
-
-useEffect(()=>{
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
-      const {uid,email, displayName, photoURL} = user;
-      dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURL:photoURL}));
-
-
-      // ...
-
-
-
-    } else {
-      // User is signed out
-      // ...
-      dispatch(removeUser());
-      
-    }
-  });
-},[]);
-
-
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
 
   return (
     <div>
-        <RouterProvider router={appRouter}/>
+      <RouterProvider router={appRouter} />
     </div>
-  )
+  );
 };
-
 export default Body;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,133 +1,112 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from 'react';
 import Header from './Header';
 import { checkValidData } from '../utils/validate';
-import {  createUserWithEmailAndPassword , signInWithEmailAndPassword, updateProfile} from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from '../utils/firebase';
-import {  useNavigate } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-const[isSignInForm, setIsSignInForm] = useState(true);
-const[errorMessage, setErrorMessage]=useState(null);
-const navigate=useNavigate();
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
 
+  const handleButtonClick = async () => {
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
 
-const name=useRef(null);
-const email=useRef(null);
-const password=useRef(null);
+    if (message) return;
 
-
-const handleButtonClick = () => {
-const message=checkValidData(email.current.value,password.current.value); // in only one line validation is done
-setErrorMessage(message);
-
-if(message) return;
-
-if(!isSignInForm){
-  
-  createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
-  .then((userCredential) => {
-     
-    const user = userCredential.user;
-    updateProfile(user, {
-      displayName: name.current.value, 
-      photoURL: "https://i.insider.com/5a43d10b4aa6b51d008b715b?width=1200&format=jpeg",
-    })
-    .then(() => {
-      
-      navigate("/browse");
-    })
-    .catch((error) => {
-      
-      setErrorMessage(error.code+"-"+error.message)
-    });
-  
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    setErrorMessage(errorCode+"-"+errorMessage)
-    // ..
-  });
-}
-else{
-  // sign in logic
-  signInWithEmailAndPassword(auth, email.current.value,password.current.value)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user);
-    navigate("/browse");
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    setErrorMessage(errorCode+"-"+errorMessage)
-  });
-}
-
-
-
-};
-
-
-  const toggleSignInform = () => {
- setIsSignInForm(!isSignInForm); 
+    if (!isSignInForm) {
+      createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: "https://i.insider.com/5a43d10b4aa6b51d008b715b?width=1200&format=jpeg",
+          })
+            .then(() => {
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(addUser({ uid, email, displayName, photoURL }));
+              navigate("/browse");
+            })
+            .catch((error) => {
+              setErrorMessage(error.code + "-" + error.message);
+            });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          const { uid, email, displayName, photoURL } = user;
+          dispatch(addUser({ uid, email, displayName, photoURL }));
+          navigate("/browse");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    }
   };
 
-
-
+  const toggleSignInform = () => {
+    setIsSignInForm(!isSignInForm); 
+  };
 
   return (
-    
     <div className='relative h-screen w-screen '>
       <Header/>
       <div className='absolute inset-0'>
-      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/20bf1f4d-1c73-48fd-8689-310d6dd80efc/81bdc063-cb8f-4afe-8a02-a3131ca4ef5e/IN-en-20240812-POP_SIGNUP_TWO_WEEKS-perspective_WEB_7998f3b6-63e3-424a-8328-550cf777ddce_large.jpg"
-      alt='Netflix back' className='object-cover h-full w-full'/>
-
- 
+        <img 
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/20bf1f4d-1c73-48fd-8689-310d6dd80efc/81bdc063-cb8f-4afe-8a02-a3131ca4ef5e/IN-en-20240812-POP_SIGNUP_TWO_WEEKS-perspective_WEB_7998f3b6-63e3-424a-8328-550cf777ddce_large.jpg"
+          alt='Netflix back' className='object-cover h-full w-full'/>
       </div>
       <form onSubmit={(e) => e.preventDefault()}
-      className=" w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg  bg-opacity-80">
-      <h1 className='font-bold text-3xl py-4 '>{isSignInForm?"Sign In":"Sign Up"}</h1>
+        className=" w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className='font-bold text-3xl py-4 '>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
 
-      {!isSignInForm && (
-          <input type='text' placeholder='full name'
-        className='p-4 my-4 w-full bg-gray-700'/>)}
+        {!isSignInForm && (
+          <input 
+            ref={name}
+            type='text' placeholder='full name'
+            className='p-4 my-4 w-full bg-gray-700'/>
+        )}
 
         <input 
-        ref={email}
-        type='text' placeholder='email address' 
-        className='p-4 my-4 w-full bg-gray-700'/>
- 
-
+          ref={email}
+          type='text' placeholder='email address' 
+          className='p-4 my-4 w-full bg-gray-700'/>
 
         <input
-        ref={password}
-        type='password' placeholder='password' 
-        className='p-4 my-4 w-full bg-gray-700'/>
+          ref={password}
+          type='password' placeholder='password' 
+          className='p-4 my-4 w-full bg-gray-700'/>
 
-<p className='text-red-800 font-bold text-lg py-2'>{errorMessage} </p>
-        
+        <p className='text-red-800 font-bold text-lg py-2'>{errorMessage} </p>
 
         <button className='p-4 my-6 bg-red-700 w-full right-0 left-0 rounded-lg' 
-        onClick={handleButtonClick}>
-          {isSignInForm?"Sign In":"Sign Up"}</button>
+          onClick={handleButtonClick}>
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
        
-       <p className='py-4 cursor-pointer' onClick={toggleSignInform}>
-
-       {isSignInForm?"new to netflix? Sign Up now":"email already in use? Sign In now"}
-         </p>
-
-
+        <p className='py-4 cursor-pointer' onClick={toggleSignInform}>
+          {isSignInForm ? "New to Netflix? Sign Up now" : "Email already in use? Sign In now"}
+        </p>
       </form>
-      
     </div>
-  )
+  );
 };
 
 export default Login;
