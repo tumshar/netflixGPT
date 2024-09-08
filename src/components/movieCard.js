d42be5f28cbcf5f38be41b1d15d812a6f3488dcd
tushar <tushar.acharrya@gmail.com>
2024-09-09 03:22:16 +0530

    g

--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -3,7 +3,7 @@ import { IMG_CDN_URL } from '../utils/constants';
 
 const MovieCard = ({posterPath}) => {
   return (
-    <div>
+    <div className='w-48 pr-6'>
         <img alt="movie Card"
         src={IMG_CDN_URL+posterPath}/>
     </div>
--- src/components/movieList.js
+++ src/components/movieList.js
@@ -4,11 +4,12 @@ import MovieCard from './movieCard';
 const MovieList = ({title, movies}) => {
 console.log(movies);
   return (
-    <div>
-        <div>
-            <h1>{title}</h1>
+    <div className='p-6'>
+          <h1 className='text-3xl font-semibold py-6'>{title}</h1>
+        <div className='flex overflow-x-scroll'>
+          
         
-        <div>
+        <div className='flex'>
             {movies?.map((movie)=>(
              <MovieCard key={movie.id} posterPath={movie.poster_path}/>
             ))}
