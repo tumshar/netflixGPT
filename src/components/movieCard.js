a2f1c09ebefbf2fb956f42b0568907f77b777303
tushar <tushar.acharrya@gmail.com>
2024-09-10 03:33:25 +0530

    movie card modification

--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -3,16 +3,16 @@ import { IMG_CDN_URL } from '../utils/constants';
 
 const MovieCard = ({ posterPath, title }) => {
   return (
-    <div className='w-64 m-3 bg-white rounded-xl overflow-hidden shadow-md hover:shadow-xl transition-all duration-300 ease-in-out'>
+    <div className='w-64 m-3 bg-gradient-to-br from-gray-100 to-gray-200 rounded-xl overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-300 ease-in-out transform hover:-translate-y-1 border border-gray-300'>
       <div className='relative pb-[150%]'>
         <img 
           alt={title || "Movie poster"}
           src={IMG_CDN_URL + posterPath}
-          className='absolute top-0 left-0 w-full h-full object-cover'
+          className='absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-300 hover:opacity-90'
         />
       </div>
       {title && (
-        <div className='p-5'>
+        <div className='p-5 border-t border-gray-300'>
           <h3 className='text-base font-semibold text-gray-800 truncate'>
             {title}
           </h3>
