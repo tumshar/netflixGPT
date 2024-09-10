9e8e26ec45d23c5ed152e5fa30ccf2c3e8de51c3
tushar <tushar.acharrya@gmail.com>
2024-09-10 20:57:02 +0530

    v

--- src/components/GPTSearch.js
+++ src/components/GPTSearch.js
@@ -4,7 +4,13 @@ import GPTMovieSuggestions from './GPTMovieSuggestions';
 
 const GPTSearch = () => {
   return (
-    <div><GPTSearchBar/>
+    <div>
+         <div className='absolute inset-0 '>
+        <img 
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/20bf1f4d-1c73-48fd-8689-310d6dd80efc/81bdc063-cb8f-4afe-8a02-a3131ca4ef5e/IN-en-20240812-POP_SIGNUP_TWO_WEEKS-perspective_WEB_7998f3b6-63e3-424a-8328-550cf777ddce_large.jpg"
+          alt='Netflix back' className='object-cover h-full w-full'/>
+      </div>
+        <GPTSearchBar/>
     <GPTMovieSuggestions/>
     </div>
   )
--- src/components/GPTSearchBar.js
+++ src/components/GPTSearchBar.js
@@ -10,31 +10,33 @@ const GPTSearchBar = () => {
 
   return (
     <div className="pt-[7%] px-4">
-      <form className='w-full md:w-2/3 mx-auto p-4 bg-black rounded-lg shadow-lg'>
-        <div className="relative flex items-center">
-          <FaSearch className="absolute left-3 text-gray-400" />
+      <form className='w-full md:w-2/3 mx-auto flex items-center space-x-2'>
+        <div className='flex-grow relative bg-white rounded-lg shadow-md'>
+          <FaSearch className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400" />
           <input 
             type='text'
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className='pl-10 pr-36 py-3 w-full rounded-l-lg bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-red-600'
+            className='pl-12 pr-24 py-4 w-full rounded-lg bg-gray-100 text-gray-800 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-red-500 focus:bg-white transition duration-300 text-lg'
             placeholder="What would you like to watch today?"
           />
-          {searchTerm && (
-            <button
-              type="button"
-              onClick={handleClear}
-              className="absolute right-32 p-1 bg-gray-600 rounded-full text-gray-400 hover:text-white hover:bg-gray-500 focus:outline-none"
-            >
-              <FaTimes size={14} />
-            </button>
-          )}
-          <FaMicrophone className="absolute right-36 text-gray-400 cursor-pointer hover:text-red-600" />
-          <button className='absolute right-0 h-full px-6 bg-red-600 text-white rounded-r-lg hover:bg-red-700 transition duration-300 font-semibold text-lg shadow-md flex items-center justify-center'>
-            <FaSearch className="mr-2" />
-            Search
-          </button>
+          <div className="absolute right-2 top-1/2 transform -translate-y-1/2 flex items-center space-x-3">
+            {searchTerm && (
+              <button
+                type="button"
+                onClick={handleClear}
+                className="p-1.5 bg-gray-200 rounded-full text-gray-600 hover:text-white hover:bg-gray-500 focus:outline-none transition duration-300"
+              >
+                <FaTimes size={14} />
+              </button>
+            )}
+            <FaMicrophone className="text-gray-400 cursor-pointer hover:text-red-500 transition duration-300" size={20} />
+          </div>
         </div>
+        <button className='px-6 py-4 bg-gradient-to-r from-red-500 to-red-600 text-white rounded-lg hover:from-red-600 hover:to-red-700 transition duration-300 font-semibold text-lg shadow-md flex items-center justify-center'>
+          <FaSearch className="mr-2" />
+          Search
+        </button>
       </form>
     </div>
   )
